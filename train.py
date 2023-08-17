2011be99e6d31574e97db25e3a02ec73bb3dd5e5
imngooh <goodjw0125@gmail.com>
2023-08-17 21:24:45 +0900

    added pcgrad

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 from tqdm.auto import tqdm
 import wandb
 from dataset import colon_train_dataloader, colon_valid_dataloader, prostate_train_dataloader, prostate_valid_dataloader, gastric_train_dataloader, gastric_valid_dataloader, total_train_dataloader, total_valid_dataloader
-
+from pcgrad import PCGrad
 
 def train_single_task(network, config, organ):
     """
@@ -238,6 +238,142 @@ def train_multi_task(network, config, mode):
             if mode == 'dann':
                 torch.save(network, base_path + f'dann_epoch_{epochs}_batch_{batch_size}_{lr}_best_acc.pt')
             else : torch.save(network, base_path + f'multi_class_epoch_{epochs}_batch_{batch_size}_{lr}_best_acc.pt')
+
+        print(
+            f'\n epoch : {epoch + 1} -- train_loss : {loss[-1]: .5f}, train_acc : {acc[-1]: .5f} valid_loss = {valid_loss[-1]:.5f}, valid_acc = {valid_acc[-1]: .5f}')
+        print(f'\t \t organ_acc : {organ_acc[-1]: .5f} organ_loss : {organ_loss[-1]: .5f} valid_organ_acc : {valid_organ_acc[-1]: .5f} valid_organ_loss : {valid_organ_loss[-1]: .5f}')
+
+
+
+
+    print('-' * 30)
+    print('-' * 30)
+    print('Train End')
+
+    return network
+
+def train_pcgrad(network, config):
+    """
+    training function for cancer + organ classification
+    with pcgrad(projection gradients) from Gradient Surgery for Multi-Task Learning
+    """
+    wandb.init(
+        project='general-cancer-classification',
+
+        config={
+            'lr': config.lr,
+            'model': 'EfficientnetB0',
+            'dataset': 'total',
+            'epochs': config.epochs,
+            'batch_size': config.batch_size,
+            'task' : 'dann with pcgrad'
+        }
+    )
+
+    TrainDataloader = total_train_dataloader(batch_size = config.batch_size, tf = config.transform)
+    ValidDataloader = total_valid_dataloader(batch_size = config.batch_size)
+
+    criterion = torch.nn.CrossEntropyLoss()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    base_path = 'C:\\Users\\User\\Desktop\\General-Cancer-Classification\\results\\'
+    epochs = config.epochs
+    lr = config.lr
+    batch_size = config.batch_size
+    optimizer = PCGrad(torch.optim.Adam(network.parameters(), lr=lr))
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(TrainDataloader)//5, 0.0001)
+
+    network.to(device)
+    loss = []
+    organ_loss = []
+
+    acc = []
+    organ_acc = []
+
+    whole_loss = []
+
+    valid_loss = []
+    valid_acc = []
+
+    valid_organ_loss = []
+    valid_organ_acc = []
+
+    valid_whole_loss = []
+
+    best_acc = 25
+
+    for epoch in tqdm(range(epochs)):
+        batch_loss = []
+        batch_acc = []
+        batch_organ_loss = []
+        batch_organ_acc = []
+        batch_total_loss = []
+
+        network.train()
+        for img, labels, organ in (iter(TrainDataloader)):
+            img, labels, organ = torch.autograd.Variable(img), torch.autograd.Variable(labels),torch.autograd.Variable(organ)
+            labels = labels.type(torch.LongTensor)
+            img, labels, organ = img.to(device), labels.to(device), organ.to(device)
+
+            optimizer.zero_grad()
+            output = network(img)
+            class_loss = criterion(output[0], labels)
+            domain_loss = criterion(output[1], organ)
+
+            losses = [class_loss, domain_loss]
+            total_loss.pc_backward(losses)
+
+            optimizer.step()
+            scheduler.step()
+
+            with torch.no_grad():
+                total_loss = class_loss + domain_loss
+                batch_loss.append(class_loss.item())
+                batch_acc.append(sum(torch.max(output[0], dim=1)[1].to(device) == labels) / img.shape[0])
+
+                batch_organ_loss.append(domain_loss.item())
+                batch_organ_acc.append(sum(torch.max(output[1], dim=1)[1].to(device) == organ) / img.shape[0])
+
+                batch_total_loss.append(total_loss.item())
+
+        loss.append((sum(batch_loss[-len(TrainDataloader):]) / len(TrainDataloader)))
+        acc.append((sum(batch_acc[-len(TrainDataloader):]) / len(TrainDataloader)))
+        organ_loss.append((sum(batch_organ_loss[-len(TrainDataloader):]) / len(TrainDataloader)))
+        organ_acc.append((sum(batch_organ_acc[-len(TrainDataloader):]) / len(TrainDataloader)))
+        whole_loss.append((sum(batch_total_loss[-len(TrainDataloader):]) / len(TrainDataloader)))
+        wandb.log({'train_cancer_acc' : acc[-1], 'train_cancer_loss' : loss[-1],'train_organ_acc' : organ_acc[-1], 'train_organ_loss' :organ_loss[-1],'train_whole_loss' : whole_loss[-1] })
+
+        network.eval()
+        for img, labels, organ in iter(ValidDataloader):
+            img, labels, organ = torch.autograd.Variable(img), torch.autograd.Variable(labels), torch.autograd.Variable(organ)
+            labels = labels.type(torch.LongTensor)
+            img, labels, organ = img.to(device), labels.to(device), organ.to(device)
+
+            with torch.no_grad():
+                output = network(img)
+                class_loss = criterion(output[0], labels)
+                domain_loss = criterion(output[1], organ)
+                total_loss = class_loss + domain_loss
+
+                batch_loss.append(class_loss.item())
+                batch_acc.append(sum(torch.max(output[0], dim=1)[1].to(device) == labels) / img.shape[0])
+
+                batch_organ_loss.append(domain_loss.item())
+                batch_organ_acc.append(sum(torch.max(output[1], dim=1)[1].to(device) == organ) / img.shape[0])
+
+                batch_total_loss.append(total_loss.item())
+
+        valid_loss.append((sum(batch_loss[-len(ValidDataloader):]) / len(ValidDataloader)))
+        valid_acc.append((sum(batch_acc[-len(ValidDataloader):]) / len(ValidDataloader)))
+        valid_organ_loss.append((sum(batch_organ_loss[-len(ValidDataloader):]) / len(ValidDataloader)))
+        valid_organ_acc.append((sum(batch_organ_acc[-len(ValidDataloader):]) / len(ValidDataloader)))
+        valid_whole_loss.append((sum(batch_total_loss[-len(ValidDataloader):]) / len(ValidDataloader)))
+        wandb.log({'valid_cancer_acc' : valid_acc[-1], 'valid_cancer_loss' : valid_loss[-1],'valid_organ_acc' : valid_organ_acc[-1], 'valid_organ_loss' :valid_organ_loss[-1],'valid_whole_loss' : valid_whole_loss[-1] })
+
+        torch.save(network, base_path + f'pcgrad_dann_task_{config.lr}_{config.transform}.pt')
+
+        if best_acc >= valid_acc[-1]:
+            best_acc = valid_acc[-1]
+            torch.save(network, base_path + f'pcgrad_dann_epoch_{epochs}_batch_{batch_size}_{lr}_best_acc.pt')
 
         print(
             f'\n epoch : {epoch + 1} -- train_loss : {loss[-1]: .5f}, train_acc : {acc[-1]: .5f} valid_loss = {valid_loss[-1]:.5f}, valid_acc = {valid_acc[-1]: .5f}')
---
+++ train_pcgrad.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import torch
+from utils import Config, seed_everything, multi_task_analysis
+from train import train_pcgrad
+from models import DANN_model
+from dataset import total_test_dataloader, colon_train_dataloader, prostate_train_dataloader, gastric_train_dataloader
+
+def pcgrad_train_test(network, config):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    network = train_pcgrad(network, config)
+
+    result = pd.DataFrame(columns=['infer', 'label', 'infer_organ', 'organ'])
+    network.to(device)
+    TestDataloader = total_test_dataloader()
+
+    print('inferring...')
+
+    for idx, i in enumerate(iter(TestDataloader)):
+        img = i[0].to(device)
+        label = i[1]
+        organ = i[2]
+        result.loc[idx] = [torch.argmax(network(img)[0]).item(), label.item(), torch.argmax(network(img)[1]).item(), organ.item()]
+
+    base_path = 'C:\\Users\\User\\Desktop\\General-Cancer-Classification\\results\\'
+
+    acc = sum(result['infer'] == result['label']) / len(result)
+    result.to_csv(base_path + f'pcgrad_dann_infer_result_acc_{acc:.5f}.csv')
+
+    multi_task_analysis(result)
+
+    return network
+
+
+if __name__ == "__main__":
+    seed_everything(42)
+    config = Config()
+    network = DANN_model
+    pcgrad_train_test(network, config)
