a36d9051664571e663c5dcfcf56e989ace68ccce
imngooh <goodjw0125@gmail.com>
2023-08-25 23:16:11 +0900

    added code for continue learning with pcgrad

--- train_pcgrad.py
+++ train_pcgrad.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import torch
 from utils import Config, seed_everything, multi_task_analysis
-from train import train_pcgrad
+from train import train_pcgrad_continued
 from models import DANN_model
 from dataset import total_test_dataloader, colon_train_dataloader, prostate_train_dataloader, gastric_train_dataloader
 
 def pcgrad_train_test(network, config):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    network = train_pcgrad(network, config)
+    network = train_pcgrad_continued(network, config)
 
     result = pd.DataFrame(columns=['infer', 'label', 'infer_organ', 'organ'])
     network.to(device)
